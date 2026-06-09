40dfd84c487074702dfc64562cdcdb7cbc9fe3b5
JuanDev27 <juan.oviedo.j@uniautonoma.edu.co>
2026-06-09 12:06:41 -0500

    Animaciones(WIP) player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,28 +6,34 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public float speed = 5f;
     private Rigidbody2D rb;
     private PlayerController playerController;
-    private Vector2 move;
+    private Vector2 moveInput;
+
+    private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerController = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {  
+        rb.velocity = moveInput * speed;
+
         if (playerController != null && !playerController.CanMove()) return;
-        move = Vector2.zero;
+        moveInput = Vector2.zero;
 
-        if (Keyboard.current.wKey.isPressed) move.y += 1f;
-        if (Keyboard.current.sKey.isPressed) move.y -= 1f;
-        if (Keyboard.current.dKey.isPressed) move.x += 1f;
-        if (Keyboard.current.aKey.isPressed) move.x -= 1f;
+        if (Keyboard.current.wKey.isPressed) moveInput.y += 1f;
+        if (Keyboard.current.sKey.isPressed) moveInput.y -= 1f;
+        if (Keyboard.current.dKey.isPressed) moveInput.x += 1f;
+        if (Keyboard.current.aKey.isPressed) moveInput.x -= 1f;
 
-        if (move.magnitude > 1) 
+        if (moveInput.magnitude > 1) 
         {
-            move.Normalize();
+            moveInput.Normalize();
         }
    
     }
@@ -40,7 +46,22 @@ public class PlayerMovement : MonoBehaviour
         // MOVIMIENTO MEDIANTE RIGIDBODY (Fluido y respeta colisiones)
         if (rb != null)
         {
-            rb.linearVelocity = move * speed;
+            rb.linearVelocity = moveInput * speed;
         }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetBool("isWalking", true);
+
+        if(context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX",moveInput.x);
+            animator.SetFloat("LastInputY",moveInput.y);
+        }
+        animator.SetFloat("InputX",moveInput.x);
+        animator.SetFloat("InputY",moveInput.y);
     }
 }
